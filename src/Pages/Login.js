cb3a65426516f87378fb6d0fbc8b3ba62812d561
JoynoolAbedinMithoon <mithun55@gmail.com>
2022-01-16 20:04:47 +0600

    login panel added

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -8,19 +8,30 @@ import { FcGoogle } from "react-icons/fc";
 function Login ()
 {
     return (
-        <div className='container'>
-            <div className='background'>
-                <img src={bg} alt="abc" />
-            </div>
-            <div>
-                <div className='login-panel'>
-                    <h2>hello from login</h2>
-                    <img src={logo} alt="logo" />
-                    <FaFacebook className='facebook-icon' />
-                    <FcGoogle className='google-icon' />
+        <main className='wrapper'>
+            <div className='container'>
+                <div className='left-side'>
+                    <img src={bg} alt="abc" width={'auto'} height={'auto'} />
+                </div>
+                <div className='right-side'>
+                    <div className='login-panel'>
+                        <img src={logo} alt="logo" />
+                        <h2>Explore new courses... Hurry up!!</h2>
+                        <form action="" className='login-form'>
+                            <input className='login-input' type="email" name="" id="" placeholder='Email Address' />
+                            <input className='login-input' type="password" name="" id="" placeholder='Password' />
+                            <button className='forget-button'>Forget Password?</button>
+                            <input type="submit" value="LOG IN" className='submit-button' />
+                        </form>
+                        <div className='form-divider line'>or</div>
+                        <div className='social-icon'>
+                            <FaFacebook className='facebook-icon' />
+                            <FcGoogle className='google-icon' />
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
+        </main>
     )
 }
 
